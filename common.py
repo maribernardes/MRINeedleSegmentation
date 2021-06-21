0dfaab77f2ed4fae18d3c3fae1fe20e26991e376
Junichi Tokuda <tokuda@bwh.harvard.edu>
2021-06-21 13:26:24 -0400

    Add argparse.

--- common.py
+++ common.py
@@ -15,6 +15,7 @@ from monai.transforms import (
     Spacingd,
     ToTensord,
 )
+from monai.utils import first, set_determinism
 from monai.networks.nets import UNet
 from monai.networks.layers import Norm
 
@@ -29,116 +30,137 @@ from monai.data import CacheDataset, DataLoader, Dataset
 # Load configurations
 #--------------------------------------------------------------------------------
 
-def getvector(config, section, key):
-    value = config.get(section, key)
-    if value:
-        value = value.split(',')
-        value = [float(s) for s in value]
-        value = tuple(value)
-        return value
-    else:
-        return None
+class Param():
 
+    def __init__(self, filename='config.ini'):
+        self.config = ConfigParser()
+        self.config.read(filename)
+        self.readParameters()
+
+    def getvector(self, config, section, key):
+        value = config.get(section, key)
+        if value:
+            value = value.split(',')
+            value = [float(s) for s in value]
+            value = tuple(value)
+            return value
+        else:
+            return None
+
+    def readParameters(self):
+ 
+        self.val_ratio = self.config.getfloat('common', 'val_ratio')
+        self.data_dir = self.config.get('common', 'data_dir')
+        self.root_dir = self.config.get('common', 'root_dir')
+        
+        self.pixel_dim = self.getvector(self.config, 'common', 'pixel_dim')
+        if self.pixel_dim == None:
+            self.pixel_dim = (1.0,1.0,1.0)
+        
+        self.window_size = self.getvector(self.config, 'common', 'window_size')
+        if self.window_size:
+            self.window_size = [int(s) for s in self.window_size]
+            self.window_size = tuple(self.window_size)
+        else:
+            self.window_size = (160,160,160)
+        
+        self.pixel_intensity_min = self.config.getfloat('common', 'pixel_intensity_min')
+        self.pixel_intensity_max = self.config.getfloat('common', 'pixel_intensity_max')
+        self.pixel_intensity_percentile_min = self.config.getfloat('common', 'pixel_intensity_percentile_min')
+        self.pixel_intensity_percentile_max = self.config.getfloat('common', 'pixel_intensity_percentile_max')
+        
+        self.model_file = self.config.get('common', 'model_file')
+
+
+class TrainingParam(Param):
     
-### Following parameters will be used for both training and testing ###
-config = ConfigParser()
-config.read('config.ini')
+    def __init__(self, filename='config.ini'):
+        super().__init__(filename)
 
-val_ratio = config.getfloat('common', 'val_ratio')
-data_dir = config.get('common', 'data_dir')
-root_dir = config.get('common', 'root_dir')
+    def readParameters(self):
+        super().readParameters()
 
-pixel_dim = getvector(config, 'common', 'pixel_dim')
-if pixel_dim == None:
-    pixel_dim = (1.0,1.0,1.0)
+        self.use_tensorboard = int(self.config.get('training', 'use_tensorboard'))
+        self.use_matplotlib = int(self.config.get('training', 'use_matplotlib'))
+        self.max_epochs = int(self.config.get('training', 'max_epochs'))
+        self.training_device_name = self.config.get('training', 'training_device_name')
 
-window_size = getvector(config, 'common', 'window_size')
-if window_size:
-    window_size = [int(s) for s in window_size]
-    window_size = tuple(window_size)
-else:
-    window_size = (160,160,160)
+        
+class InferenceParam(Param):
+    
+    def __init__(self, filename='config.ini'):
+        super().__init__(filename)
 
-pixel_intensity_min = config.getfloat('common', 'pixel_intensity_min')
-pixel_intensity_max = config.getfloat('common', 'pixel_intensity_max')
-pixel_intensity_percentile_min = config.getfloat('common', 'pixel_intensity_percentile_min')
-pixel_intensity_percentile_max = config.getfloat('common', 'pixel_intensity_percentile_max')
+    def readParameters(self):
+        super().readParameters()
 
-model_file = config.get('common', 'model_file')
-
-use_tensorboard = int(config.get('training', 'use_tensorboard'))
-use_matplotlib = int(config.get('training', 'use_matplotlib'))
-max_epochs = int(config.get('training', 'max_epochs'))
-
-training_device_name = config.get('training', 'training_device_name')
-inference_device_name = config.get('inference', 'inference_device_name')
+        self.inference_device_name = self.config.get('inference', 'inference_device_name')
 
 
 #--------------------------------------------------------------------------------
 # Validation transform
 #--------------------------------------------------------------------------------
 
-val_transforms = Compose(
-    [
-        LoadImaged(keys=["image", "label"]),
-        AddChanneld(keys=["image", "label"]),
-        Spacingd(keys=["image", "label"], pixdim=pixel_dim, mode=("bilinear", "nearest")),
-        Orientationd(keys=["image", "label"], axcodes="LPS"),
-        ScaleIntensityRanged(
-            keys=["image"], a_min=pixel_intensity_min, a_max=pixel_intensity_max,
-            b_min=0.0, b_max=1.0, clip=True,
-        ),
-        # ScaleIntensityRangePercentilesd(
-        #     keys=["image"], lower=pixel_intensity_percentile_min, upper=pixel_intensity_percentile_max,
-        #     b_min=0.0, b_max=1.0, clip=True,
-        # ),
-        CropForegroundd(keys=["image", "label"], source_key="image"),
-        ToTensord(keys=["image", "label"]),
-    ]
-)
+def loadValidationTransforms(param):
+    
+    val_transforms = Compose(
+        [
+            LoadImaged(keys=["image", "label"]),
+            AddChanneld(keys=["image", "label"]),
+            Spacingd(keys=["image", "label"], pixdim=param.pixel_dim, mode=("bilinear", "nearest")),
+            Orientationd(keys=["image", "label"], axcodes="LPS"),
+            ScaleIntensityRanged(
+                keys=["image"], a_min=param.pixel_intensity_min, a_max=param.pixel_intensity_max,
+                b_min=0.0, b_max=1.0, clip=True,
+            ),
+            # ScaleIntensityRangePercentilesd(
+            #     keys=["image"], lower=param.pixel_intensity_percentile_min, upper=param.pixel_intensity_percentile_max,
+            #     b_min=0.0, b_max=1.0, clip=True,
+            # ),
+            CropForegroundd(keys=["image", "label"], source_key="image"),
+            ToTensord(keys=["image", "label"]),
+        ]
+    )
+    return val_transforms
 
 
-#--------------------------------------------------------------------------------
-# Validation transform
-#--------------------------------------------------------------------------------
 
-print('Reading data from: ' + data_dir)
+# def getLoader(param, data_dicts, transforms):
+#     
+#     ds = CacheDataset(
+#         data=data_dicts, transform=transforms, cache_rate=1.0, num_workers=4)
+#     # val_ds = Dataset(data=val_files, transform=val_transforms)
+#     loader = DataLoader(ds, batch_size=1, num_workers=4)
+# 
+#     # # TODO: THe following lines are not needed.
+#     # check_ds = Dataset(data=data_dicts, transform=transforms)
+#     # check_loader = DataLoader(check_ds, batch_size=1)
+#     # print(check_loader)
+#     # check_data = first(check_loader)
+#     # image, label = (check_data["image"][0][0], check_data["label"][0][0])
+#     # print(f"image shape: {image.shape}, label shape: {label.shape}")
+#     
+#     return loader
 
-train_images = sorted(glob.glob(os.path.join(data_dir, "images", "*.nii.gz")))
-train_labels = sorted(glob.glob(os.path.join(data_dir, "labels", "*.nii.gz")))
-
-data_dicts = [
-    {"image": image_name, "label": label_name}
-    for image_name, label_name in zip(train_images, train_labels)
-]
-
-n_total = len(data_dicts)
-n_val = round(n_total * val_ratio)
-
-print('Total data size:      ' + str(n_total))
-print('Validation data size: ' + str(n_val))
-
-train_files, val_files = data_dicts[:-n_val], data_dicts[-n_val:]
-
-val_ds = CacheDataset(
-    data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=4)
-# val_ds = Dataset(data=val_files, transform=val_transforms)
-val_loader = DataLoader(val_ds, batch_size=1, num_workers=4)
 
 
 #--------------------------------------------------------------------------------
 # Model
 #--------------------------------------------------------------------------------
 
-model_unet = UNet(
-    dimensions=3,
-    in_channels=1,
-    out_channels=2,
-    channels=(16, 32, 64, 128, 256),
-    strides=(2, 2, 2, 2),
-    num_res_units=2,
-    norm=Norm.BATCH,
-)
+def setupModel():
 
-post_pred = AsDiscrete(argmax=True, to_onehot=True, n_classes=2)
-post_label = AsDiscrete(to_onehot=True, n_classes=2)
+    model_unet = UNet(
+        dimensions=3,
+        in_channels=1,
+        out_channels=2,
+        channels=(16, 32, 64, 128, 256),
+        strides=(2, 2, 2, 2),
+        num_res_units=2,
+        norm=Norm.BATCH,
+    )
+    
+    post_pred = AsDiscrete(argmax=True, to_onehot=True, n_classes=2)
+    post_label = AsDiscrete(to_onehot=True, n_classes=2)
+
+    return (model_unet, post_pred, post_label)
--- inference.py
+++ inference.py
@@ -26,74 +26,145 @@ import tempfile
 import shutil
 import os
 import glob
+import sys
+import argparse
+
 from configparser import ConfigParser
 
 from common import *
 
 
-#--------------------------------------------------------------------------------
-# Model
-#--------------------------------------------------------------------------------
 
-device = torch.device(inference_device_name)
+def run(param, input_path, output_path, image_type):
 
-model = model_unet.to(device)
+    val_transforms =  loadValidationTransforms(param)
 
-#dice_metric = DiceMetric(include_background=True, reduction="mean")
-#post_trans = Compose([Activations(sigmoid=True), AsDiscrete(threshold_values=True)])
-
-#model.load_state_dict(torch.load(os.path.join(root_dir, "best_metric_model.pth")))
-model.load_state_dict(torch.load(os.path.join(root_dir, model_file)))
-
-
-#--------------------------------------------------------------------------------
-# Validate
-#--------------------------------------------------------------------------------
-
-model.eval()
-
-with torch.no_grad():
-
-    saver = NiftiSaver(output_dir="./output")
-    metric_sum = 0.0
-    metric_count = 0
+    print('Reading data from: ' + param.data_dir)
     
-    for i, val_data in enumerate(val_loader):
-        roi_size = window_size
-        sw_batch_size = 4
+    train_images = sorted(glob.glob(os.path.join(param.data_dir, "images", "*.nii.gz")))
+    train_labels = sorted(glob.glob(os.path.join(param.data_dir, "labels", "*.nii.gz")))
+    
+    data_dicts = [
+        {"image": image_name, "label": label_name}
+        for image_name, label_name in zip(train_images, train_labels)
+    ]
+    
+    n_total = len(data_dicts)
+    n_val = round(n_total * param.val_ratio)
+    
+    print('Total data size:      ' + str(n_total))
+    print('Validation data size: ' + str(n_val))
+    
+    train_files, val_files = data_dicts[:-n_val], data_dicts[-n_val:]
 
-        val_images, val_labels = val_data["image"].to(device), val_data["label"].to(device)
-        val_outputs = sliding_window_inference(val_images, roi_size, sw_batch_size, model)
-        val_outputs = post_pred(val_outputs)
-        #val_outputs = post_trans(val_outputs)        
-        val_labels = post_label(val_labels)
-        value = compute_meandice(
-            y_pred=val_outputs,
-            y=val_labels,
-            include_background=False,
-        )
-        print(value)
-        metric_count += len(value)
-        metric_sum += value.sum().item()        
-        #metric_sum += value.item() * len(value)
-        
-        # # plot the slice [:, :, 80]
-        # sl = 15
-        # plt.figure("check", (18, 6))
-        # plt.subplot(1, 3, 1)
-        # plt.title(f"image {i}")
-        # plt.imshow(val_data["image"][0, 0, :, :, sl], cmap="gray")
-        # plt.subplot(1, 3, 2)
-        # plt.title(f"label {i}")
-        # plt.imshow(val_data["label"][0, 0, :, :, sl])
-        # plt.subplot(1, 3, 3)
-        # plt.title(f"output {i}")
-        # plt.imshow(torch.argmax(
-        #     val_outputs, dim=1).detach().cpu()[0, :, :, sl])
-        # plt.show()
-        val_output_label = torch.argmax(val_outputs, dim=1, keepdim=True)
-        saver.save_batch(val_output_label, val_data['image_meta_dict'])
-        
-    metric = metric_sum / metric_count
-    print("evaluation metric:", metric)
+    #val_loader = getValidationLoader(param)
+    #val_loader = getLoader(param, val_files, val_transforms)
 
+    val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=4)
+    # val_ds = Dataset(data=val_files, transform=val_transforms)
+    val_loader = DataLoader(val_ds, batch_size=1, num_workers=4)
+    
+
+    #--------------------------------------------------------------------------------
+    # Model
+    #--------------------------------------------------------------------------------
+
+    (model_unet, post_pred, post_label) = setupModel()
+    
+    device = torch.device(param.inference_device_name)
+    model = model_unet.to(device)
+    
+    #dice_metric = DiceMetric(include_background=True, reduction="mean")
+    #post_trans = Compose([Activations(sigmoid=True), AsDiscrete(threshold_values=True)])
+    
+    #model.load_state_dict(torch.load(os.path.join(root_dir, "best_metric_model.pth")))
+    model.load_state_dict(torch.load(os.path.join(param.root_dir, param.model_file)))
+    
+    
+    #--------------------------------------------------------------------------------
+    # Validate
+    #--------------------------------------------------------------------------------
+    
+    model.eval()
+    
+    with torch.no_grad():
+    
+        saver = NiftiSaver(output_dir=output_path)
+        metric_sum = 0.0
+        metric_count = 0
+        
+        for i, val_data in enumerate(val_loader):
+            roi_size = param.window_size
+            sw_batch_size = 4
+    
+            val_images, val_labels = val_data["image"].to(device), val_data["label"].to(device)
+            val_outputs = sliding_window_inference(val_images, roi_size, sw_batch_size, model)
+            val_outputs = post_pred(val_outputs)
+            #val_outputs = post_trans(val_outputs)        
+            val_labels = post_label(val_labels)
+            value = compute_meandice(
+                y_pred=val_outputs,
+                y=val_labels,
+                include_background=False,
+            )
+            print(value)
+            metric_count += len(value)
+            metric_sum += value.sum().item()        
+            #metric_sum += value.item() * len(value)
+            
+            # # plot the slice [:, :, 80]
+            # sl = 15
+            # plt.figure("check", (18, 6))
+            # plt.subplot(1, 3, 1)
+            # plt.title(f"image {i}")
+            # plt.imshow(val_data["image"][0, 0, :, :, sl], cmap="gray")
+            # plt.subplot(1, 3, 2)
+            # plt.title(f"label {i}")
+            # plt.imshow(val_data["label"][0, 0, :, :, sl])
+            # plt.subplot(1, 3, 3)
+            # plt.title(f"output {i}")
+            # plt.imshow(torch.argmax(
+            #     val_outputs, dim=1).detach().cpu()[0, :, :, sl])
+            # plt.show()
+            val_output_label = torch.argmax(val_outputs, dim=1, keepdim=True)
+            saver.save_batch(val_output_label, val_data['image_meta_dict'])
+            
+        metric = metric_sum / metric_count
+        print("evaluation metric:", metric)
+
+
+def main(argv):
+  try:
+    parser = argparse.ArgumentParser(description="Apply a saved DL model for segmentation.")
+    parser.add_argument('cfg', metavar='CONFIG_FILE', type=str, nargs=1,
+                        help='Configuration file')
+    parser.add_argument('input', metavar='INPUT_PATH', type=str, nargs=1,
+                        help='A file or a folder that contains images.')
+    parser.add_argument('output', metavar='OUTPUT_PATH', type=str, nargs=1,
+                        help='A folder to store the output file(s).')
+    parser.add_argument('-t', dest='type', default='folder',
+                        help="Image type ('file': a file; 'folder': a folder containing multiple images.)")
+            
+    args = parser.parse_args(argv)
+
+    config_file = args.cfg[0]
+    input_path = args.input[0]
+    output_path = args.output[0]
+    image_type = args.type
+
+    # Make the destination directory, if it does not exists.
+    #os.makedirs(output_path, exist_ok=True)
+
+    print('Loading parameters from: ' + config_file)
+    param = InferenceParam(config_file)
+
+    run(param, input_path, output_path, image_type)
+
+
+  except Exception as e:
+    print(e)
+  sys.exit()
+
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
