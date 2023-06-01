459f2fc5d69bc5f595971601560ec07b31c52f89
maribernardes <maribernardes@gmail.com>
2023-06-01 10:38:13 -0400

    Fixed bug in shuffle. No longer repeats files from training in the test group

--- shuffle.py
+++ shuffle.py
@@ -6,52 +6,64 @@ import os
 import sys
 import argparse
 
-def shuffle_dataset(src_dir, dst_dir, training_split, dir_prefix):
+import os
+import shutil
 
+def create_folder(folder_path, clean_files=False):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    else:
+        if clean_files:
+            # Clean files inside the folder
+            file_list = os.listdir(folder_path)
+            for file_name in file_list:
+                file_path = os.path.join(folder_path, file_name)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+
+def shuffle_dataset(src_dir, dst_dir, training_split, dir_prefix, file_prefix):
+
+  # Randomly shuffle the cases
   random.seed()
-  
   cases = list(range(0,sum(training_split)))
   random.shuffle(cases)
-  
-  
+        
   # Clean the destination folders
   for p in dir_prefix:
-    dir_name = '%s/%s_images' % (dst_dir, p)
-    shutil.rmtree(dir_name)
-    os.mkdir(dir_name)
-    dir_name = '%s/%s_labels' % (dst_dir, p)
-    shutil.rmtree(dir_name)
-    os.mkdir(dir_name)
+    dst_dir_image = '%s/%s_images' % (dst_dir, p)
+    create_folder(dst_dir_image, clean_files=True)
+    dst_dir_label = '%s/%s_labels' % (dst_dir, p)
+    create_folder(dst_dir_label, clean_files=True)
   
+  # Initialize previous group final index as 0
+  end_index = 0
   
-  training_split = [0] + training_split
-    
-  # Copy files
+  # Copy files for each group folder
   for group in range(3): 
     print('Copying %s dataset...' % dir_prefix[group])
     dst_dir_image = '%s/%s_images' % (dst_dir, dir_prefix[group])
     dst_dir_label = '%s/%s_labels' % (dst_dir, dir_prefix[group])
-  
-    start_index = training_split[group]
-    end_index = training_split[group]+training_split[group+1]
+
+    # Get start and end index for current group
+    start_index = end_index
+    end_index = start_index + training_split[group]
     for i in range(start_index, end_index):
       # images files
-      filename1 = 'PhantomSynthetic_%s_M.nii.gz' % str(cases[i]).zfill(3) 
-      filename2 = 'PhantomSynthetic_%s_P.nii.gz' % str(cases[i]).zfill(3) 
+      filename1 = file_prefix + '_'+ str(cases[i]).zfill(3) + '_M.nii.gz'
+      filename2 = file_prefix + '_'+ str(cases[i]).zfill(3) + '_P.nii.gz'
       src_path1 = '%s/%s' % (src_dir, filename1)
       src_path2 = '%s/%s' % (src_dir, filename2)
       shutil.copy(src_path1, dst_dir_image)
       shutil.copy(src_path2, dst_dir_image)
       
       # label file
-      filename = 'PhantomSynthetic_%s_label.nii.gz' % str(cases[i]).zfill(3) 
+      filename = file_prefix + '_'+ str(cases[i]).zfill(3) + '_label.nii.gz'
       src_path = '%s/%s' % (src_dir, filename)
       shutil.copy(src_path, dst_dir_label)
 
 
 def main(argv):
 
-  #src_dir = 'data_pool'
   dir_prefix = ['train', 'val', 'test']
   training_split = [10, 6, 8]
 
@@ -62,6 +74,8 @@ def main(argv):
                         help='Data pool folder.')
     parser.add_argument('dst', metavar='DST_DIR', type=str, nargs=1,
                         help='Destination directory.')
+    parser.add_argument('-p', "--prefix", default='t2', 
+                        help="Files common prefix")
     parser.add_argument('-t', dest='nTraining', default=training_split[0],
                         help='Number of training images (default: %d)' % training_split[0])
     parser.add_argument('-v', dest='nValidation', default=training_split[1],
@@ -80,12 +94,12 @@ def main(argv):
   training_split[0]  = int(args.nTraining)
   training_split[1]  = int(args.nValidation)
   training_split[2]  = int(args.nTesting)
+  file_prefix = args.prefix
   
   # Make the destination directory, if it does not exists.
   os.makedirs(dst_dir, exist_ok=True)
-  shuffle_dataset(src_dir, dst_dir, training_split, dir_prefix)
+  shuffle_dataset(src_dir, dst_dir, training_split, dir_prefix, file_prefix)
 
   
 if __name__ == "__main__":
   main(sys.argv[1:])
-    
